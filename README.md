fb6e4398887e56256680bd0f31c6ed9f0a9d1186
polaris <polaris-@users.noreply.github.com>
2014-04-06 07:04:08 -0400

    Added compatibility to readme, updated db for PS0

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ A Nintendo DS (and possibly Wii) server emulator.
 Open source projects referenced during the creation of this project:
 OpenSpy Core - https://github.com/sfcspanky/Openspy-Core/
 Luigi Auriemma's Gslist and enctypex_decoder - http://aluigi.altervista.org/papers.htm
+
+
+Compatibility:
+The following games have been tested with varying results.
+
+Tetris DS (U) - Friends only games are playable. Worldwide matchmaking does not work.
+Jump Ultimate Stars (J) - Friends only games are playable. Worldwide matchmaking does not work. Voice untested.
+Phantasy Star Zero (U) - Connects to server, "Play Alone" option works. Friends can be added but cannot play with each other yet. The gsps.nitnendowifi.net server must be implemented.
+
+Matchmaking as a whole right now has not been implemented so it will most likely not work in any game yet.
--- gamespy/gs_database.py
+++ gamespy/gs_database.py
@@ -19,7 +19,7 @@ class GamespyDatabase(object):
             # but I'm not good with databases and I'm not 100% positive that, for instance, that all
             # user id's will be ints, or all passwords will be ints, etc, despite not seeing any
             # evidence yet to say otherwise as far as Nintendo DS games go.
-            c.execute('''CREATE TABLE users (profileid INT, userid TEXT, password TEXT, email TEXT, uniquenick TEXT, pid TEXT, lon TEXT, lat TEXT, loc TEXT, lastname TEXT, stat TEXT)''')
+            c.execute('''CREATE TABLE users (profileid INT, userid TEXT, password TEXT, email TEXT, uniquenick TEXT, pid TEXT, lon TEXT, lat TEXT, loc TEXT, lastname TEXT, stat TEXT, partnerid TEXT)''')
             c.execute('''CREATE TABLE sessions (session TEXT, profileid INT)''')
             c.execute('''CREATE TABLE buddies (userProfileid INT, buddyProfileId INT, status INT)''')
             self.conn.commit()
@@ -111,6 +111,7 @@ class GamespyDatabase(object):
             loc = ""  # Always blank?
             lastname = ""
             stat = ""
+            partnerid = ""
 
             # Hash password before entering it into the database.
             # For now I'm using a very simple MD5 hash.
@@ -120,8 +121,8 @@ class GamespyDatabase(object):
             password = md5.hexdigest()
 
             c = self.conn.cursor()
-            c.execute("INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?,?,?)",
-                      [profileid, str(userid), password, email, uniquenick, pid, lon, lat, loc, lastname, stat])
+            c.execute("INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+                      [profileid, str(userid), password, email, uniquenick, pid, lon, lat, loc, lastname, stat, partnerid])
             c.close()
 
             self.conn.commit()
