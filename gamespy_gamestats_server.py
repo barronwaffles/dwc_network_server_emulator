6d70e354ae12c6a856f19249ad93be19ae43ff96
Matt Soucy <msoucy@csh.rit.edu>
2014-05-23 03:31:02 -0400

    Take advantage of Python builtins

--- gamespy_gamestats_server.py
+++ gamespy_gamestats_server.py
@@ -22,9 +22,6 @@ logger = utils.create_logger(logger_name, logger_filename, -1, logger_output_to_
 address = ("0.0.0.0", 29920)
 
 class GameSpyGamestatsServer(object):
-    def __init__(self):
-        pass
-
     def start(self):
         endpoint_search = serverFromString(reactor, "tcp:%d:interface=%s" % (address[1], address[0]))
         conn_search = endpoint_search.listen(GamestatsFactory())
@@ -60,33 +57,27 @@ class Gamestats(LineReceiver):
 
         self.lid = "0"
 
-    def log(self, level, message):
-        if self.session == "":
-            if self.gameid == "":
-                logger.log(level, "[%s:%d] %s", self.address.host, self.address.port,message)
-            else:
-                logger.log(level, "[%s:%d | %s] %s", self.address.host, self.address.port, self.gameid, message)
-        else:
-            if self.gameid == "":
-                logger.log(level, "[%s:%d | %s] %s", self.address.host, self.address.port, self.session, message)
-            else:
-                logger.log(level, "[%s:%d | %s | %s] %s", self.address.host, self.address.port, self.session, self.gameid, message)
+    def log(self, level, message, *args):
+        gameid = (" | %s"%self.gameid) if self.gameid else ""
+        sessid = (" | %s"%self.session) if self.gameid else ""
+        logger.log(level, "[%s:%d%s%s] %s", self.address.host, self.address.port, sessid, gameid, message, *args)
 
     def connectionMade(self):
-        self.log(logging.INFO, "Received connection from %s:%d" % (self.address.host, self.address.port))
+        self.log(logging.INFO, "Received connection from %s:%d", self.address.host, self.address.port)
 
         # Generate a random challenge string
         self.challenge = utils.generate_random_str(10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
         # The first command sent to the client is always a login challenge containing the server challenge key.
-        msg_d = []
-        msg_d.append(('__cmd__', "lc"))
-        msg_d.append(('__cmd_val__', "1"))
-        msg_d.append(('challenge', self.challenge))
-        msg_d.append(('id', "1"))
+        msg_d = [
+            ('__cmd__', "lc"),
+            ('__cmd_val__', "1"),
+            ('challenge', self.challenge),
+            ('id', "1"),
+        ]
         msg = gs_query.create_gamespy_message(msg_d)
 
-        self.log(logging.DEBUG, "SENDING: '%s'..." % msg)
+        self.log(logging.DEBUG, "SENDING: '%s'...", msg)
 
         msg = self.crypt(msg)
         self.transport.write(bytes(msg))
@@ -103,25 +94,22 @@ class Gamestats(LineReceiver):
         commands, self.remaining_message = gs_query.parse_gamespy_message(msg)
         #logger.debug("STATS RESPONSE: %s" % msg)
 
+        cmds = {
+            "auth": self.perform_auth,
+            "authp": self.perform_authp,
+            "ka": self.perform_ka,
+            "setpd": self.perform_setpd,
+            "getpd": self.perform_getpd,
+            "newgame": self.perform_newgame,
+            "updgame": self.perform_updgame,
+        }
+        def cmderr(data_parsed):
+            logger.debug("Found unknown command, don't know how to handle '%s'.", data_parsed['__cmd__'])
+
         for data_parsed in commands:
             print data_parsed
 
-            if data_parsed['__cmd__'] == "auth":
-                self.perform_auth(data_parsed)
-            elif data_parsed['__cmd__'] == "authp":
-                self.perform_authp(data_parsed)
-            elif data_parsed['__cmd__'] == "ka":
-                self.perform_ka(data_parsed)
-            elif data_parsed['__cmd__'] == "setpd":
-                self.perform_setpd(data_parsed, msg)
-            elif data_parsed['__cmd__'] == "getpd":
-                self.perform_getpd(data_parsed)
-            elif data_parsed['__cmd__'] == "newgame":
-                self.perform_newgame(data_parsed)
-            elif data_parsed['__cmd__'] == "updgame":
-                self.perform_updgame(data_parsed)
-            else:
-                logger.debug("Found unknown command, don't know how to handle '%s'." % data_parsed['__cmd__'])
+            cmds.get(data_parsed['__cmd__'], cmderr)(data_parsed)
 
     def perform_auth(self, data_parsed):
         self.log(logging.DEBUG, "Parsing 'auth'...")
@@ -131,15 +119,16 @@ class Gamestats(LineReceiver):
 
         self.session = utils.generate_random_number_str(10)
 
-        msg_d = []
-        msg_d.append(('__cmd__', "lc"))
-        msg_d.append(('__cmd_val__', "2"))
-        msg_d.append(('sesskey', self.session))
-        msg_d.append(('proof', 0))
-        msg_d.append(('id', "1"))
+        msg_d = [
+            ('__cmd__', "lc"),
+            ('__cmd_val__', "2"),
+            ('sesskey', self.session),
+            ('proof', 0),
+            ('id', "1"),
+        ]
         msg = gs_query.create_gamespy_message(msg_d)
 
-        self.log(logging.DEBUG, "SENDING: '%s'..." % msg)
+        self.log(logging.DEBUG, "SENDING: '%s'...", msg)
 
         msg = self.crypt(msg)
         self.transport.write(bytes(msg))
@@ -171,36 +160,25 @@ class Gamestats(LineReceiver):
         email = uniquenick + "@nds" # The Wii also seems to use @nds.
 
         # Wii: Serial number
+        csnum = ""
         if "csnum" in authtoken_parsed:
             csnum = authtoken_parsed['csnum']
             console = 1
-        else:
-            csnum = ""
 
         # Wii: Friend code
+        cfc = ""
         if "cfc" in authtoken_parsed:
             cfc = authtoken_parsed['cfc']
             console = 1
-        else:
-            cfc = ""
 
         # NDS: Wifi network's BSSID
-        if "bssid" in authtoken_parsed:
-            bssid = authtoken_parsed['bssid']
-        else:
-            bssid = ""
+        bssid = authtoken_parsed.get('bssid', '')
 
         # NDS: Device name
-        if "devname" in authtoken_parsed:
-            devname = authtoken_parsed['devname']
-        else:
-            devname = ""
+        devname = authtoken_parsed.get('devname', '')
 
         # NDS: User's birthday
-        if "birth" in authtoken_parsed:
-            birth = authtoken_parsed['birth']
-        else:
-            birth = ""
+        birth = authtoken_parsed.get('birth', '')
 
         valid_user = self.db.check_user_exists(userid, gsbrcd)
         profileid = None
@@ -217,51 +195,50 @@ class Gamestats(LineReceiver):
 
             self.sessions[profileid] = self
 
-            msg_d = []
-            msg_d.append(('__cmd__', "pauthr"))
-            msg_d.append(('__cmd_val__', profileid))
-            msg_d.append(('lid', self.lid))
+            msg_d = [
+                ('__cmd__', "pauthr"),
+                ('__cmd_val__', profileid),
+                ('lid', self.lid),
+            ]
             msg = gs_query.create_gamespy_message(msg_d)
 
             self.profileid = int(profileid)
 
-            self.log(logging.DEBUG, "SENDING: '%s'..." % msg)
+            self.log(logging.DEBUG, "SENDING: '%s'...", msg)
 
             msg = self.crypt(msg)
             self.transport.write(bytes(msg))
-        else:
-            # Return error
-            pass
 
     def perform_ka(self, data_parsed):
-        msg_d = []
-        msg_d.append(('__cmd__', "ka"))
-        msg_d.append(('__cmd_val__', ""))
+        msg_d = [
+            ('__cmd__', "ka"),
+            ('__cmd_val__', ""),
+        ]
         msg = gs_query.create_gamespy_message(msg_d)
 
-        self.log(logging.DEBUG, "SENDING: '%s'..." % msg)
+        self.log(logging.DEBUG, "SENDING: '%s'...", msg)
 
         msg = self.crypt(msg)
         self.transport.write(bytes(msg))
-        return
 
     def perform_setpd(self, data_parsed, data):
-        msg_d = []
-        msg_d.append(('__cmd__', "setpdr"))
-        msg_d.append(('__cmd_val__', 1))
-        msg_d.append(('lid', self.lid))
-        msg_d.append(('pid', self.profileid))
-        msg_d.append(('mod', int(time.time())))
+        msg_d = [
+            ('__cmd__', "setpdr"),
+            ('__cmd_val__', 1),
+            ('lid', self.lid),
+            ('pid', self.profileid),
+            ('mod', int(time.time())),
+        ]
         msg = gs_query.create_gamespy_message(msg_d)
 
-        self.log(logging.DEBUG, "SENDING: '%s'..." % msg)
+        self.log(logging.DEBUG, "SENDING: '%s'...", msg)
 
         msg = self.crypt(msg)
         self.transport.write(bytes(msg))
 
         # TODO: Return error message.
         if int(data_parsed['pid']) != self.profileid:
-            logger.warning("ERROR: %d tried to update %d's profile" % (int(data_parsed['pid']), self.profileid))
+            logger.warning("ERROR: %d tried to update %d's profile", int(data_parsed['pid']), self.profileid)
             return
 
         data_str = "\\data\\"
@@ -292,23 +269,22 @@ class Gamestats(LineReceiver):
             profile_data = profile_data[0][0]
 
         for key in keys:
-            if key != "__cmd__" and key != "__cmd_val__" and key != "":
-                data += "\\"
-                data += key
-                data += "\\"
+            if key not in ("__cmd__", "__cmd_val__", ""):
+                data += "\\" + key + "\\"
                 if key in profile_data:
                     data += profile_data[key]
 
         modified = int(time.time())
 
-        msg_d = []
-        msg_d.append(('__cmd__', "getpdr"))
-        msg_d.append(('__cmd_val__', 1))
-        msg_d.append(('lid', self.lid))
-        msg_d.append(('pid', self.profileid))
-        msg_d.append(('mod', modified))
-        msg_d.append(('length', len(data)))
-        msg_d.append(('data', data))
+        msg_d = [
+            ('__cmd__', "getpdr"),
+            ('__cmd_val__', 1),
+            ('lid', self.lid),
+            ('pid', self.profileid),
+            ('mod', modified),
+            ('length', len(data)),
+            ('data', data),
+        ]
         msg = gs_query.create_gamespy_message(msg_d)
 
         self.log(logging.DEBUG, "SENDING: '%s'..." % msg)
