de70413b6ab95b1fa0bd8343f90188480a76fb55
polaris <polaris-@users.noreply.github.com>
2014-06-06 16:27:17 -0400

    Start stats server on startup

--- master_server.py
+++ master_server.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         #GameSpyServerBrowserServer,
         GameSpyNatNegServer,
         NasServer,
-        #InternalStatsServer,
+        InternalStatsServer,
         StorageServer,
     ]
     for server in servers:
