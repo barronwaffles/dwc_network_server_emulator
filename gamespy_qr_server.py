d33c97ec85b977cc6df1a76da41c3c39082ade59
smtddr <minusworld@hellokitty.com>
2014-09-04 21:22:38 +0000

    Reduce timeout wait
    Official servers dropped servers after 1min of silence.

--- gamespy_qr_server.py
+++ gamespy_qr_server.py
@@ -390,7 +390,7 @@ class GameSpyQRServer(object):
 
         for session_id in self.sessions:
             delta = now - self.sessions[session_id].keepalive
-            timeout = 60 * 5 # Remove clients that haven't responded in x seconds
+            timeout = 61 # Remove clients that haven't responded in x seconds
 
             if delta < 0 or delta >= timeout:
                 pruned.append(session_id)
