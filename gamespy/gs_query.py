cc11fbc7dbc7d176264189656da1a27e35af070e
smtddr <minusworld@hellokitty.com>
2014-09-10 14:42:25 +0000

    Remove nulls from fields
    As it turns out TvC puts a nullbyte at the end of its data field
    when doing setpd. There's probably somewhere upstream where
    it could be handle before reaching this parsing code, but I'll
    just do this for now.

--- gamespy/gs_query.py
+++ gamespy/gs_query.py
@@ -50,7 +50,7 @@ def parse_gamespy_message(message):
                 messages['__cmd_val__'] = value
                 found_command = True
 
-            messages[key] = value
+            messages[key] = value.replace('\x00','')
 
         stack.append(messages)
         messages = {}
