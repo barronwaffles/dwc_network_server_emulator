7081f07d3f244642070add90ec2d91764792b6d8
polaris- <polaris-@users.noreply.github.com>
2014-11-04 08:06:43 -0500

    Don't filter further if returned list is empty
    Fixes an exception when looking for a specific key in _list.txt but no data is returned. For example, searching for MYSTERY_S for gamecd IRAO.

--- nas_server.py
+++ nas_server.py
@@ -223,13 +223,15 @@ class NasHTTPServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                         if os.path.isfile(os.path.join(dlcpath, "_list.txt")):
                             ret = self.filter_list(safeloadfi("_list.txt"), attr1, attr2, attr3)
                             
-                            # this is super game-specific, but we need to handle gen 5 mystery gifts properly somehow
-                            if post["gamecd"].startswith("IRA") and attr1.startswith("MYSTERY"):
-                                ret = self.filter_list_g5_mystery_gift(ret, post["rhgamecd"])
-                                ret = self.filter_list_by_date(ret, post["token"])
-                            
-                            if post["gamecd"] in gamecodes_return_random_file:
-                                ret = self.filter_list_by_date(ret, post["token"])
+                            # Don't try to filter any further if the list is empty.
+                            if ret.strip() != "":
+                                # this is super game-specific, but we need to handle gen 5 mystery gifts properly somehow
+                                if post["gamecd"].startswith("IRA") and attr1.startswith("MYSTERY"):
+                                    ret = self.filter_list_g5_mystery_gift(ret, post["rhgamecd"])
+                                    ret = self.filter_list_by_date(ret, post["token"])
+                                
+                                if post["gamecd"] in gamecodes_return_random_file:
+                                    ret = self.filter_list_by_date(ret, post["token"])
 
                 if action == "contents":
                     # Get only the base filename just in case there is a path involved somewhere in the filename string.
