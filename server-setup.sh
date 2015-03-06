8c015122472971ab824b3a827e695b31e300bf51
Kyle Warwick-Mathieu <BeanJr@users.noreply.github.com>
2015-03-06 01:59:43 -0500

    added some git fixes for SSL errors

--- server-setup.sh
+++ server-setup.sh
@@ -27,6 +27,7 @@ echo "For example: /etc/apache2"
 read -e APACHEDIR
 echo "The path your provided is: $APACHEDIR"
 echo "Now I will clone the github repo to the directory of where this script is"
+git config --global http.sslVerify false
 git clone https://github.com/BeanJr/dwc_network_server_emulator
 echo "Now that that's out of the way, let's do some apache stuff"
 cd "$APACHEDIR"
