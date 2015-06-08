53cab34a97d872ed3338b796f42cac97b85dbf2d
XeR <XeR@Forgotten-Legends.org>
2015-06-08 10:31:36 +0200

    Fix checks for "<!DOCTYPE" instead of "DOCTYPE"

--- storage_server.py
+++ storage_server.py
@@ -303,7 +303,7 @@ class StorageHTTPServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             
             ret = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body>'
             
-            if "DOCTYPE" in post.upper():
+            if "<!DOCTYPE" in post.upper():
                 logger.log(logging.ERROR, "User tried to redefine a DOCTYPE")
                 return
 
